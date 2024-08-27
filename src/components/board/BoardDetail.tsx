29725cff4d81d7f439ccd597cb0894f3365fbbf0
changhee <changhee@logiall.com>
2024-08-27 09:28:23 +0900

    로컬스토리지 로직을 제거한다

--- src/components/board/BoardDetail.tsx
+++ src/components/board/BoardDetail.tsx
@@ -16,22 +16,11 @@ export default function BoardDetail({ id }: BoardRegistProps) {
 
   useEffect(() => {
     if (id) {
-      const boardList = localStorage.getItem('formData');
-
-      if (boardList) {
-        const parsedData = JSON.parse(boardList);
-        const foundItem = parsedData.find(
-          (item: BoradDataType) => item.number === Number(id)
-        );
-
-        if (foundItem) {
-          setFormData({
-            number: foundItem.number,
-            title: foundItem.title,
-            content: foundItem.content,
-          });
-        }
-      }
+      setFormData({
+        number: 1,
+        title: 'title',
+        content: 'content',
+      });
     }
   }, [id]);
 
@@ -46,46 +35,17 @@ export default function BoardDetail({ id }: BoardRegistProps) {
 
   const onClickSubmitButton = (event: React.FormEvent<HTMLButtonElement>) => {
     event.preventDefault();
+    postBoard(formData);
+    resetForm();
 
-    // 로컬 스토리지에 저장된 데이터 가져오기
-    const dataList = getBoardList();
-
-    // 동일한 번호가 있는지 확인
-    const isNumber = dataList.some(
-      (data: BoradDataType) => data.number === formData.number
-    );
-
-    if (!isNumber) {
-      dataList.push(formData);
-      postBoard(formData);
-      localStorage.setItem('formData', JSON.stringify(dataList));
-      resetForm();
-      return;
-    }
-
-    alert('번호중복 등록 실패했습니다');
+    return;
   };
 
   const onClickModifyButton = (event: React.FormEvent<HTMLButtonElement>) => {
     event.preventDefault();
-
-    const dataList = getBoardList();
-    const updatedData = dataList.map((item: BoradDataType) =>
-      item.number === formData.number ? formData : item
-    );
-
-    // 수정된 데이터를 로컬 스토리지에 다시 저장
-    localStorage.setItem('formData', JSON.stringify(updatedData));
-
     if (id) {
       updateBoard({ id, data: formData });
     }
-  };
-
-  const getBoardList = () => {
-    const savedData = localStorage.getItem('formData');
-
-    return savedData ? JSON.parse(savedData) : [];
   };
 
   return (
